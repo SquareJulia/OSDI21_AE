fix: aggregate formula; feat: save npy for sparsert

GNNAdvisor/GNNA_main.py
@@ -14,29 +14,46 @@ from dataset import *
 
 parser = argparse.ArgumentParser()
 # Dataset related parameters.
-parser.add_argument("--dataDir", type=str, default="../osdi-ae-graphs", help="the path to graphs")
-parser.add_argument("--dataset", type=str, default='amazon0601', help="dataset")
-parser.add_argument("--dim", type=int, default=96, help="input embedding dimension size")
-parser.add_argument("--hidden", type=int, default=16, help="hidden dimension size")
-parser.add_argument("--classes", type=int, default=22, help="output classes size")
+parser.add_argument("--dataDir", type=str,
+                    default="../osdi-ae-graphs", help="the path to graphs")
+parser.add_argument("--dataset", type=str,
+                    default='amazon0601', help="dataset")
+parser.add_argument("--dim", type=int, default=96,
+                    help="input embedding dimension size")
+parser.add_argument("--hidden", type=int, default=16,
+                    help="hidden dimension size")
+parser.add_argument("--classes", type=int, default=22,
+                    help="output classes size")
 
 # Model training related parameters.
-parser.add_argument('--model', type=str, default='gcn', choices=['gcn', 'gin'],  help="GCN or GIN")
-parser.add_argument("--num_epoches", type=int, default=200, help="number of epoches for training, default=200")
+parser.add_argument('--model', type=str, default='gcn',
+                    choices=['gcn', 'gin'],  help="GCN or GIN")
+parser.add_argument("--num_epoches", type=int, default=200,
+                    help="number of epoches for training, default=200")
 
 # Manually set the performance related parameters
-parser.add_argument("--partSize", type=int, default=32, help="neighbor-group size")
-parser.add_argument("--dimWorker", type=int, default=32, help="number of worker threads (MUST < 32)")
-parser.add_argument("--warpPerBlock", type=int, default=4, help="number of warp per block, recommended: GCN: 8, GIN: 2")
-parser.add_argument("--sharedMem", type=int, default=100, help="shared memory size of each block (Quadro P6000 64(KB) sm_61), default=100(KB) for RTX3090 sm_86")
+parser.add_argument("--partSize", type=int, default=32,
+                    help="neighbor-group size")
+parser.add_argument("--dimWorker", type=int, default=32,
+                    help="number of worker threads (MUST < 32)")
+parser.add_argument("--warpPerBlock", type=int, default=4,
+                    help="number of warp per block, recommended: GCN: 8, GIN: 2")
+parser.add_argument("--sharedMem", type=int, default=100,
+                    help="shared memory size of each block (Quadro P6000 64(KB) sm_61), default=100(KB) for RTX3090 sm_86")
 
 # Additional flags for studies.
-parser.add_argument('--manual_mode', type=str, choices=['True', 'False'], default='True', help="True: use manual config, False: auto config, default: True")
-parser.add_argument('--verbose_mode', type=str, choices=['True', 'False'], default='False', help="True: verbose mode, False: simple mode, default: False")
-parser.add_argument('--enable_rabbit', type=str, choices=['True', 'False'], default='False', help="True: enable rabbit reordering, False, disable rabbit reordering, default: False (disable for both manual and auto mode).")
-parser.add_argument('--loadFromTxt', type=str, choices=['True', 'False'], default='False', help="True: load the graph TXT edge list, False: load from .npy, default: False (load from npz fast)")
-parser.add_argument('--single_spmm', type=str, choices=['True', 'False'], default='False', help="True: profile the single SpMM (neighbor aggregation) kernel for number epoches times")
-parser.add_argument('--verify_spmm', type=str, choices=['True', 'False'], default='False', help="True: verify the output correctness of a single SpMM (neighbor aggregation) kernel against the CPU reference implementation.")
+parser.add_argument('--manual_mode', type=str, choices=[
+                    'True', 'False'], default='True', help="True: use manual config, False: auto config, default: True")
+parser.add_argument('--verbose_mode', type=str, choices=[
+                    'True', 'False'], default='False', help="True: verbose mode, False: simple mode, default: False")
+parser.add_argument('--enable_rabbit', type=str, choices=['True', 'False'], default='False',
+                    help="True: enable rabbit reordering, False, disable rabbit reordering, default: False (disable for both manual and auto mode).")
+parser.add_argument('--loadFromTxt', type=str, choices=['True', 'False'], default='False',
+                    help="True: load the graph TXT edge list, False: load from .npy, default: False (load from npz fast)")
+parser.add_argument('--single_spmm', type=str, choices=['True', 'False'], default='False',
+                    help="True: profile the single SpMM (neighbor aggregation) kernel for number epoches times")
+parser.add_argument('--verify_spmm', type=str, choices=['True', 'False'], default='False',
+                    help="True: verify the output correctness of a single SpMM (neighbor aggregation) kernel against the CPU reference implementation.")
 
 args = parser.parse_args()
 print(args)
@@ -58,24 +75,33 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 ####################################
 if loadFromTxt:
     path = osp.join(args.dataDir, args.dataset)
-    dataset = custom_dataset(path, args.dim, args.classes, load_from_txt=True, verbose=verbose_mode)
+    dataset = custom_dataset(path, args.dim, args.classes,
+                             load_from_txt=True, verbose=verbose_mode)
 else:
     path = osp.join(args.dataDir, args.dataset+".npz")
-    dataset = custom_dataset(path, args.dim, args.classes, load_from_txt=False, verbose=verbose_mode)
+    dataset = custom_dataset(path, args.dim, args.classes,
+                             load_from_txt=False, verbose=verbose_mode)
 
 num_nodes = dataset.num_nodes
 num_edges = dataset.num_edges
 column_index = dataset.column_index
 row_pointers = dataset.row_pointers
 degrees = dataset.degrees
+if verbose_mode:
+    print('degrees:')
+    print(degrees)
+    print('column_index:')
+    print(column_index)
+    print('row_pointers:')
+    print(row_pointers)
 
 ####################################
 # Building input property profile.
 ####################################
-inputInfo = inputProperty(row_pointers, column_index, degrees, 
-                            partSize, dimWorker, warpPerBlock, sharedMem,
-                            hiddenDim=args.hidden, dataset_obj=dataset, enable_rabbit=enable_rabbit,
-                            manual_mode=manual_mode, verbose=verbose_mode)
+inputInfo = inputProperty(row_pointers, column_index, degrees,
+                          partSize, dimWorker, warpPerBlock, sharedMem,
+                          hiddenDim=args.hidden, dataset_obj=dataset, enable_rabbit=enable_rabbit,
+                          manual_mode=manual_mode, verbose=verbose_mode)
 
 ####################################
 # Decider for parameter selection.
@@ -96,29 +122,40 @@ if verbose_mode:
 # sys.exit(0)
 
 ####################################
+# SparseRT
+####################################
+
+print(' ==== sparse Matrix for sparseRT')
+srtInputFile = dataset.save_for_sprt(inputInfo.modeBarrier)
+
+####################################
 # Building neighbor partitioning.
 ####################################
 start = time.perf_counter()
-partPtr, part2Node = GNNA.build_part(inputInfo.partSize, inputInfo.row_pointers)
+partPtr, part2Node = GNNA.build_part(
+    inputInfo.partSize, inputInfo.row_pointers)
 build_neighbor_parts = time.perf_counter() - start
 if verbose_mode:
     print("# Build nb_part (s): {:.3f}".format(build_neighbor_parts))
 
-inputInfo.row_pointers  = inputInfo.row_pointers.to(device)
-inputInfo.column_index  = inputInfo.column_index.to(device)
+inputInfo.row_pointers = inputInfo.row_pointers.to(device)
+inputInfo.column_index = inputInfo.column_index.to(device)
 inputInfo.partPtr = partPtr.int().to(device)
-inputInfo.part2Node  = part2Node.int().to(device)
+inputInfo.part2Node = part2Node.int().to(device)
 
+if verbose_mode:
+    print('degrees after:')
+    print(degrees)
 ####################################
 # Verifing a single SpMM kernel
 # against the CPU reference.
 ####################################
 if verify_spmm:
     from unitest import *
-    valid = Verification(args.hidden, \
-                        inputInfo.row_pointers, inputInfo.column_index, degrees, \
-                        inputInfo.partPtr, inputInfo.part2Node, \
-                        partSize, dimWorker, warpPerBlock)
+    valid = Verification(args.hidden,
+                         inputInfo.row_pointers, inputInfo.column_index, degrees,
+                         inputInfo.partPtr, inputInfo.part2Node,
+                         partSize, dimWorker, warpPerBlock)
     valid.compute()
     valid.reference(dataset.edge_index, dataset.val, dataset.num_nodes)
     valid.compare()
@@ -129,10 +166,10 @@ if verify_spmm:
 ####################################
 if single_spmm:
     from unitest import *
-    valid = Verification(args.hidden, \
-                        inputInfo.row_pointers, inputInfo.column_index, degrees, \
-                        inputInfo.partPtr, inputInfo.part2Node, \
-                        partSize, dimWorker, warpPerBlock)
+    valid = Verification(args.hidden,
+                         inputInfo.row_pointers, inputInfo.column_index, degrees,
+                         inputInfo.partPtr, inputInfo.part2Node,
+                         partSize, dimWorker, warpPerBlock)
     valid.profile_spmm(round=args.num_epoches)
     sys.exit(0)
 
@@ -179,12 +216,15 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 ####################################
 # Define training function.
 ####################################
+
+
 def train():
     model.train()
     optimizer.zero_grad()
     loss = F.nll_loss(model()[:], dataset.y[:])
     loss.backward()
     optimizer.step()
+
 
 if __name__ == '__main__':
     # dry run

GNNAdvisor/dataset.py
@@ -2,11 +2,12 @@
 import torch
 import numpy as np
 import time
-import dgl 
+import dgl
 import os.path as osp
 
 from scipy.sparse import *
 import rabbit
+
 
 def func(x):
     '''
@@ -17,21 +18,25 @@ def func(x):
     else:
         return 1
 
+
 class custom_dataset(torch.nn.Module):
     """
     data loading for more graphs
     """
+
     def __init__(self, path, dim, num_class, load_from_txt=True, verbose=False):
         super(custom_dataset, self).__init__()
 
         self.nodes = set()
 
         self.load_from_txt = load_from_txt
+        self.path = path
+
         self.num_nodes = 0
-        self.num_features = dim 
+        self.num_features = dim
         self.num_classes = num_class
         self.edge_index = None
-        
+
         self.reorder_flag = False
         self.verbose_flag = verbose
 
@@ -45,9 +50,12 @@ class custom_dataset(torch.nn.Module):
         train = 1
         val = 0.3
         test = 0.1
-        self.train_mask = [1] * int(self.num_nodes * train) + [0] * (self.num_nodes  - int(self.num_nodes * train))
-        self.val_mask = [1] * int(self.num_nodes * val)+ [0] * (self.num_nodes  - int(self.num_nodes * val))
-        self.test_mask = [1] * int(self.num_nodes * test) + [0] * (self.num_nodes  - int(self.num_nodes * test))
+        self.train_mask = [1] * int(self.num_nodes * train) + \
+            [0] * (self.num_nodes - int(self.num_nodes * train))
+        self.val_mask = [1] * int(self.num_nodes * val) + \
+            [0] * (self.num_nodes - int(self.num_nodes * val))
+        self.test_mask = [1] * int(self.num_nodes * test) + \
+            [0] * (self.num_nodes - int(self.num_nodes * test))
         self.train_mask = torch.BoolTensor(self.train_mask).cuda()
         self.val_mask = torch.BoolTensor(self.val_mask).cuda()
         self.test_mask = torch.BoolTensor(self.test_mask).cuda()
@@ -68,9 +76,7 @@ class custom_dataset(torch.nn.Module):
                 dst_li.append(dst)
                 self.nodes.add(src)
                 self.nodes.add(dst)
-            
-            # self.g.add_edges(src_li, dst_li)
-            self.num_edges = len(src_li)
+
             self.num_nodes = max(self.nodes) + 1
             self.edge_index = np.stack([src_li, dst_li])
 
@@ -79,7 +85,7 @@ class custom_dataset(torch.nn.Module):
                 print("# Loading (txt) {:.3f}s ".format(dur))
 
         # loading from a .npz graph file
-        else: 
+        else:
             if not path.endswith('.npz'):
                 raise ValueError("graph file must be a .npz file")
 
@@ -89,13 +95,11 @@ class custom_dataset(torch.nn.Module):
             dst_li = graph_obj['dst_li']
 
             self.num_nodes = graph_obj['num_nodes']
-            # self.g.add_edges(src_li, dst_li)
-            self.num_edges = len(src_li)
-            self.edge_index = np.stack([src_li, dst_li])
             dur = time.perf_counter() - start
             if self.verbose_flag:
                 print("# Loading (npz)(s): {:.3f}".format(dur))
-        
+
+        self.num_edges = len(src_li)
         self.avg_degree = self.num_edges / self.num_nodes
         self.avg_edgeSpan = np.mean(np.abs(np.subtract(src_li, dst_li)))
 
@@ -104,22 +108,14 @@ class custom_dataset(torch.nn.Module):
             print("# avg_degree: {:.2f}".format(self.avg_degree))
             print("# avg_edgeSpan: {}".format(int(self.avg_edgeSpan)))
 
-        # Build graph CSR.
+        # plus Identity matrix
+        src_li = src_li+[i for i in range(self.num_nodes)]
+        dst_li = dst_li+[i for i in range(self.num_nodes)]
+        self.edge_index = np.stack([src_li, dst_li])
+        self.num_edges += self.num_nodes
+
         self.val = [1] * self.num_edges
-        start = time.perf_counter()
-        scipy_coo = coo_matrix((self.val, self.edge_index), shape=(self.num_nodes, self.num_nodes))
-        scipy_csr = scipy_coo.tocsr()
-        build_csr = time.perf_counter() - start
-
-        if self.verbose_flag:
-            print("# Build CSR after reordering (s): {:.3f}".format(build_csr))
-
-        self.column_index = torch.IntTensor(scipy_csr.indices)
-        self.row_pointers = torch.IntTensor(scipy_csr.indptr)
-
-        # Get degrees array.
-        degrees = (self.row_pointers[1:] - self.row_pointers[:-1]).tolist()
-        self.degrees = torch.sqrt(torch.FloatTensor(list(map(func, degrees)))).cuda()
+        self.generate_csr_and_degrees()
 
     def init_embedding(self, dim):
         '''
@@ -127,7 +123,7 @@ class custom_dataset(torch.nn.Module):
         Called from __init__.
         '''
         self.x = torch.randn(self.num_nodes, dim).cuda()
-    
+
     def init_labels(self, num_class):
         '''
         Generate the node label.
@@ -157,22 +153,68 @@ class custom_dataset(torch.nn.Module):
                 print("# Reorder time (s): {}".format(reorder_time))
                 print("Reordered edge_index\n", self.edge_index)
 
-            # Rebuild a new graph CSR according to the updated edge_index
-            val = [1] * self.num_edges
-            start = time.perf_counter()
-            
-            scipy_coo = coo_matrix((val, self.edge_index), shape=(self.num_nodes, self.num_nodes))
-            scipy_csr = scipy_coo.tocsr()
-            self.column_index = torch.IntTensor(scipy_csr.indices)
-            self.row_pointers = torch.IntTensor(scipy_csr.indptr)
-            build_csr = time.perf_counter() - start
+            self.generate_csr_and_degrees()
 
-            # Re-generate degrees array.
-            degrees = (self.row_pointers[1:] - self.row_pointers[:-1]).tolist()
-            self.degrees = torch.sqrt(torch.FloatTensor(list(map(func, degrees)))).cuda()
+    def generate_csr_and_degrees(self):
+        '''
+        Generate a new graph CSR and degrees array according to the updated edge_index.
+        '''
+        start = time.perf_counter()
 
-            if self.verbose_flag:
-                print("# Re-Build CSR (s): {:.3f}".format(build_csr))
+        scipy_coo = coo_matrix((self.val, self.edge_index),
+                               shape=(self.num_nodes, self.num_nodes))
+        self.a_hat = scipy_coo.toarray().astype('float32')
+        scipy_csr = scipy_coo.tocsr()
+        self.column_index = torch.IntTensor(scipy_csr.indices)
+        self.row_pointers = torch.IntTensor(scipy_csr.indptr)
+        build_csr = time.perf_counter() - start
+
+        # Re-generate degrees array.
+        degrees = (self.row_pointers[1:] - self.row_pointers[:-1]).tolist()
+        self.degrees = (1.0/torch.sqrt(torch.FloatTensor(
+            list(map(func, degrees))))).cuda()
+
+        if self.verbose_flag:
+            print("# Re-Build CSR (s): {:.3f}".format(build_csr))
+
+    def a_times_d_for_sprt(self, modeBarrier):
+        '''
+        compute DAD for [0, modeBarrier) lines of A_hat.
+        '''
+        self.a_hat = self.a_hat[0:modeBarrier]
+        for row in range(modeBarrier):
+            for col_idx_idx in range(self.row_pointers[row], self.row_pointers[row+1]):
+                col = self.column_index[col_idx_idx].item()
+                self.a_hat[row][col] *= self.degrees[row].item()
+                self.a_hat[row][col] *= self.degrees[col].item()
+
+    def save_for_sprt(self, modeBarrier):
+        '''
+        Save [0,modeBarrier) lines of A_hat(times degrees_hat) in '.npy' format for sparseRT.
+        Return: file name(.npy).
+        '''
+        if modeBarrier == 0:
+            return ''
+        if self.verbose_flag:
+            print('------save for sparseRT')
+            print('=== original A_hat:')
+            print(self.a_hat)
+        self.a_times_d_for_sprt(modeBarrier)
+        if self.verbose_flag:
+            print('=== after times D:')
+            print(self.a_hat)
+
+        npy_path = '../sprt/npys/'
+        if self.path.startswith('../'):  # only osdi-ae-graphs/zsy-test-graphs
+            npy_path += self.path.split('../')[1].split('.')[0]
+        else:
+            npy_path += self.path.split('/')[-1].split('.')[0]
+        npy_path += '.npy'
+        if self.verbose_flag:
+            print('=== npy path:')
+            print(npy_path)
+        np.save(npy_path, self.a_hat.transpose())
+        return npy_path
 
 
 if __name__ == '__main__':
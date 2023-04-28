25ea07f9442768dc237b476a18369fe11e311d32
xiaosiyier <xiaosiyier@outlook.com>
2023-04-28 14:21:41 +0000

    test:tiny modification

--- GNNAdvisor/0_run_gcn.sh
+++ GNNAdvisor/0_run_gcn.sh
@@ -2,5 +2,8 @@ mkdir logs
 mv *.csv logs/
 mv *.log logs/
 ./bench_GCN.py --preOrRun pre
-./bench_GCN.py --preOrRun run|tee GCN.log
+for i in `seq 1 20`
+    do
+        ./bench_GCN.py --preOrRun run|tee -a GCN.log
+    done
 ./1_log2csv.py GCN.log
